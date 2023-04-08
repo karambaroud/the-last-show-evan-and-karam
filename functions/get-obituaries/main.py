fd22f46a2a23e908906be35058bd6fce152eb98b
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-04-08 17:53:35 -0600

    Update main.py
    Modified the this .py file such that we don't have to worry about email verification; the user does not input any email on the front-end.

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -1,5 +1,4 @@
 # add your get-obituaries function here
-# add your get-obituaries function here
 
 # add your get-notes function here
 
@@ -9,34 +8,25 @@ from boto3.dynamodb.conditions import Key
 import urllib.request
 
 dynamodb_resource = boto3.resource("dynamodb")
-table = dynamodb_resource.Table("lotion-30146985")
+table = dynamodb_resource.Table("obituaries-30146985")
 
 def get_handler(event, context):
-    email = event["headers"]["email"]
+    
     access_token = event["headers"]["authorization"].split()[1]
 
-    url = "https://www.googleapis.com/oauth2/v2/userinfo"
-
-    req = urllib.request.Request(url, headers={"Authorization": f"OAuth {access_token}"})
-    response =  urllib.request.urlopen(req)
+    
+    response =  urllib.request.urlopen()
     data = json.loads(response.read().decode())
 
-    if data["email"] != email:
-        return {
-            "statusCode": 401,
-            "body": json.dumps({
-                "message": "Unauthorized"
-            })
-        }
     
     queryParameter = event["queryStringParameters"]
+
     try:
-        res = table.query(KeyConditionExpression=Key("email").eq(queryParameter["email"]))
         return {
             "statusCode": 200,
                 "body": json.dumps({
                     "message": "success",
-                    "notes": res
+                    "notes": "something"
                 })
         }
     except Exception as exp:
